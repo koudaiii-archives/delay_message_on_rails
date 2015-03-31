4591f59476e9c91fc811ab81803c52d04900835d
koudaiii <cs006061@gmail.com>
2015-03-31 23:46:01 +0900

    remove sent, fix name(delay_until_time)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -69,6 +69,6 @@ class MessagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
-      params.require(:message).permit(:recipient_email, :text, :delay_until_time, :timezone_offset, :sent)
+      params.require(:message).permit(:recipient_email, :text, :delay_until_time, :timezone_offset)
     end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ActiveRecord::Base
+  validates :recipient_email, :text, :delay_until_time, :timezone_offset, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :recipient_email, format: { with: VALID_EMAIL_REGEX, message: 'is not valid' }
 end
