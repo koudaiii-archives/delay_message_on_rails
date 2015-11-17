4599193d0850efbd4463fe2069fc41c7c3c8bdca
koudaiii <cs006061@gmail.com>
2015-11-17 19:21:22 +0900

    development use sqlite and rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331133743) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20151117092338) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "recipient_email"
@@ -24,6 +21,13 @@ ActiveRecord::Schema.define(version: 20150331133743) do
     t.boolean  "sent",             default: false
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
