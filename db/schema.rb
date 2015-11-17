38a45ceda9c1ecd635008d5479df93c78adf1426
koudaiii <cs006061@gmail.com>
2015-11-17 16:47:57 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150331133743) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string   "recipient_email"
     t.text     "text"
